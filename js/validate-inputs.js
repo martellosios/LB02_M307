29c8a69611c68a699603b4a7722459281b934af8
affolterj <82879197+affolterj@users.noreply.github.com>
2022-05-24 20:26:35 +0200

    test

--- js/validate-inputs.js
+++ js/validate-inputs.js
@@ -1,11 +1,13 @@
 // read form element
-const vorname = document.getElementById(elementId: 'vorname');
-const nachname = document.getElementById(elementId: "nachname");
-const email = document.getElementById(elementId: "email");
-const passwort = document.getElementById(elementId: "passwort");
+const form = document.getElementById(elementId: 'form');
+const geschlecht = document.getElementById(elementId 'geschlecht');
+const vorname = document.getElementById(elementId 'vorname');
+const nachname = document.getElementById(elementId 'nachname');
+const email = document.getElementById(elementId 'email');
+const sprachen = document.getElementById(elementId 'sprachen');
 
 // Event Listeners
-form.addEventListener(type: "submit" , listener: function (e:Event){
+form.addEventListener(type: 'submit' , listener: function (e:Event){
     e.preventDefault();
     alert("test");
 });
