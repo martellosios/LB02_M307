dcffa1bf697e4c53211e72bb7f70f91f839429c7
martellosios <martellosios@bzz.ch>
2022-05-26 12:15:00 +0200

    js

--- js/validate-inputs.js
+++ js/validate-inputs.js
@@ -5,35 +5,35 @@ const vorname = document.getElementById("vorname");
 const nachname = document.getElementById("nachname");
 const email = document.getElementById("email");
 const sprachen = document.getElementById("sprachen");
+const agb = document.getElementById("agb");
 
 // Check required fields
-function checkRequired(input){
-    let isRequired = false;
-    if (input.value.trim() === ''){
-        //error
-        console.log(`${input.id} is required!`);
-        isRequired = true;
-    } else {
-        console.log(`${input.id} is provided!`);
-    }
-    return isRequired;
+function checkRequired(input) {
+  let isRequired = false;
+  if (input.value.trim() === "") {
+    //error
+    console.log(`${input.id} is required!`);
+    isRequired = true;
+  } else {
+    console.log(`${input.id} is provided!`);
+  }
+  return isRequired;
 }
 
 // Validate form input elements
-function validateForm(){
-    //check required inputs
-    if (!checkRequired(vorname)){
-        //success .. proceed with program  
-    } else {
-        //missing input
-        console.log("Missing input");
-    }
+function validateForm() {
+  //check required inputs
+  if (!checkRequired(agb)) {
+    //success .. proceed with program
+  } else {
+    //missing input
+    console.log("Missing input");
+  }
 }
 
 // Event Listeners
-form.addEventListener('submit' , function(e){
-    e.preventDefault();
-    //First Validate form
-    validateForm();
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  //First Validate form
+  validateForm();
 });
-
